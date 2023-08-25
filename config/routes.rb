99defab47b852efc1d6f6dbcb283936f5af77b05
Alicja Surzyn <alicja.surzyn@gmail.com>
2023-08-25 13:37:56 -0600

    "more updates!"

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root to: "lists#index"
   # root "articles#index"
   resources :movies, only: %i[index show] do
   end
